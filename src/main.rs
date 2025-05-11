00b0505a14adc344ec0d9369214e59bac70f6e2c
Siddharth Singh <me@shantaram.xyz>
2025-05-11 13:16:30 +0530

    implement (rust-side) flags and config system

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,29 @@ use std::{
     path::{Path, PathBuf},
     process,
 };
+use stupid_simple_kv::{Kv, MemoryBackend, SqliteBackend};
 
 mod fs;
+
+#[derive(Debug)]
+enum TetronError {
+    Other(String),
+    IdentifierNotFound,
+}
+
+impl std::fmt::Display for TetronError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TetronError::Other(s) => write!(f, "tetron: error: ${s}"),
+            TetronError::IdentifierNotFound => write!(
+                f,
+                "tetron: error: An identifier was expected in game.json but not found."
+            ),
+        }
+    }
+}
+
+impl std::error::Error for TetronError {}
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -40,12 +61,14 @@ fn normalize_path(path: &Path) -> Result<PathBuf, anyhow::Error> {
     Ok(full_path.canonicalize()?)
 }
 
-struct Game {
+struct Game<'a> {
     path: PathBuf,
     fs: OverlayFS,
+    pub(crate) config: Kv<'a>,
+    pub(crate) flags: Kv<'a>,
 }
 
-impl TryFrom<TetronArgs> for Game {
+impl<'a> TryFrom<TetronArgs> for Game<'a> {
     type Error = anyhow::Error;
 
     fn try_from(args: TetronArgs) -> Result<Self, Self::Error> {
@@ -63,17 +86,41 @@ impl TryFrom<TetronArgs> for Game {
             layers.push(fs::to_vfs_layer(layer)?);
         }
 
+        let fs = OverlayFS::from_layers(layers);
+
+        let json = fs.read_text_file("game.json")?;
+        let config = Kv::from_json_string(Box::new(MemoryBackend::new()), json)?;
+
+        let identifier: String = config
+            .get(&("identifier",))?
+            .ok_or(TetronError::IdentifierNotFound)?
+            .try_into()?;
+        // TODO: implement wasm backend (probably use IndexedDB or localstorage)
+        #[cfg(not(target_arch = "wasm32"))]
+        let backend = {
+            let data = dirs::data_dir().ok_or(TetronError::Other(String::from(
+                "Error getting user data dir",
+            )))?;
+
+            let db_path = data.join("tetron").join(identifier);
+            std::fs::create_dir_all(&db_path)?;
+            SqliteBackend::file(&db_path.join("flags.db"))?
+        };
+
+        let flags = Kv::new(Box::new(backend));
+
         Ok(Self {
             path: game_path,
-            fs: OverlayFS::from_layers(layers),
+            fs,
+            config,
+            flags,
         })
     }
 }
 
-impl Game {
+impl<'a> Game<'a> {
     fn read_text_file(&self, path: &str) -> Result<String, anyhow::Error> {
-        let buf = self.fs.open_file(path)?;
-        Ok(String::from_utf8(buf)?)
+        Ok(self.fs.read_text_file(path)?)
     }
 }
 
@@ -81,9 +128,12 @@ pub fn main() -> Result<(), anyhow::Error> {
     let args = TetronArgs::parse();
     let game = Game::try_from(args)?;
     println!("{:#?}", game.path);
-    let value = game.read_text_file("game.json")?;
-    println!("{value}");
-    let listing = game.fs.read_dir("")?;
-    println!("{listing:#?}");
+
+    println!(
+        "{:#?} {:#?}",
+        game.config.get(&("flag1"))?,
+        game.config.get(&("entrypoint"))?
+    );
+
     Ok(())
 }
