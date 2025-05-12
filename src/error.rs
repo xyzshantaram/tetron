cf6475337d45d0c1c2e26fa54b2c822ff254eb3f
Siddharth Singh <me@shantaram.xyz>
2025-05-12 22:36:28 +0530

    add common Rhai errors to TetronError

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,36 @@
+use rhai::{EvalAltResult, Position};
+
 #[derive(Debug)]
 pub enum TetronError {
     Other(String),
     IdentifierNotFound,
-    JsError(String),
+    ModuleNotFound(String, Position),
+    RhaiRuntime(String, Option<Position>),
 }
 
 impl From<String> for TetronError {
     fn from(value: String) -> Self {
         Self::Other(value)
+    }
+}
+
+impl From<TetronError> for Box<EvalAltResult> {
+    fn from(value: TetronError) -> Self {
+        match value {
+            TetronError::Other(e) => Box::new(EvalAltResult::ErrorRuntime(
+                format!("Unknown error: {e}").into(),
+                rhai::Position::NONE,
+            )),
+            TetronError::IdentifierNotFound => panic!("This should never happen"),
+            TetronError::ModuleNotFound(e, pos) => Box::new(EvalAltResult::ErrorModuleNotFound(
+                format!("Module not found: {e}"),
+                pos,
+            )),
+            TetronError::RhaiRuntime(e, pos) => Box::new(EvalAltResult::ErrorRuntime(
+                format!("Runtime error: {e}").into(),
+                pos.unwrap_or_default(),
+            )),
+        }
     }
 }
 
@@ -19,7 +42,17 @@ impl std::fmt::Display for TetronError {
                 f,
                 "tetron: error: An identifier was expected in game.json but not found."
             ),
-            TetronError::JsError(s) => write!(f, "JavaScript runtime error: {s}"),
+            TetronError::ModuleNotFound(e, position) => {
+                write!(
+                    f,
+                    "tetron: module not found: error {e} at position {position}"
+                )
+            }
+            TetronError::RhaiRuntime(e, pos) => write!(
+                f,
+                "tetron: runtime error: {e} at position {}",
+                pos.unwrap_or_default()
+            ),
         }
     }
 }
