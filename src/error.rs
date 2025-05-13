070d2474fd7bd8a637ea952214aee96a697005d4
Siddharth Singh <me@shantaram.xyz>
2025-05-13 18:45:22 +0530

    add more error kinds to TetronError

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,16 @@
 use rhai::{EvalAltResult, Position};
+use stupid_simple_kv::KvError;
+
+use crate::fs::FsError;
 
 #[derive(Debug)]
 pub enum TetronError {
     Other(String),
-    IdentifierNotFound,
+    RequiredConfigNotFound(String),
     ModuleNotFound(String, Position),
     RhaiRuntime(String, Option<Position>),
+    KvError(String),
+    FsError(String),
 }
 
 impl From<String> for TetronError {
@@ -21,7 +26,7 @@ impl From<TetronError> for Box<EvalAltResult> {
                 format!("Unknown error: {e}").into(),
                 rhai::Position::NONE,
             )),
-            TetronError::IdentifierNotFound => panic!("This should never happen"),
+            TetronError::RequiredConfigNotFound(_) => panic!("This should never happen"),
             TetronError::ModuleNotFound(e, pos) => Box::new(EvalAltResult::ErrorModuleNotFound(
                 format!("Module not found: {e}"),
                 pos,
@@ -29,6 +34,14 @@ impl From<TetronError> for Box<EvalAltResult> {
             TetronError::RhaiRuntime(e, pos) => Box::new(EvalAltResult::ErrorRuntime(
                 format!("Runtime error: {e}").into(),
                 pos.unwrap_or(Position::NONE),
+            )),
+            TetronError::KvError(s) => Box::new(EvalAltResult::ErrorRuntime(
+                format!("Key-value storage error: {s}").into(),
+                Position::NONE,
+            )),
+            TetronError::FsError(s) => Box::new(EvalAltResult::ErrorRuntime(
+                format!("Overlay filesystem error: {s}").into(),
+                Position::NONE,
             )),
         }
     }
@@ -38,9 +51,9 @@ impl std::fmt::Display for TetronError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             TetronError::Other(s) => write!(f, "tetron: error: ${s}"),
-            TetronError::IdentifierNotFound => write!(
+            TetronError::RequiredConfigNotFound(var) => write!(
                 f,
-                "tetron: error: An identifier was expected in game.json but not found."
+                "tetron: error: The required config item \"{var}\" was expected in game.json but not found."
             ),
             TetronError::ModuleNotFound(e, position) => {
                 write!(
@@ -53,7 +66,21 @@ impl std::fmt::Display for TetronError {
                 "tetron: runtime error: '{e}' at position {}",
                 pos.unwrap_or(Position::NONE)
             ),
+            TetronError::KvError(s) => write!(f, "Key-value storage error: {s}"),
+            TetronError::FsError(s) => write!(f, "Overlay filesystem error: {s}"),
         }
+    }
+}
+
+impl From<KvError> for TetronError {
+    fn from(value: KvError) -> Self {
+        TetronError::KvError(value.to_string())
+    }
+}
+
+impl From<FsError> for TetronError {
+    fn from(value: FsError) -> Self {
+        Self::FsError(value.to_string())
     }
 }
 
