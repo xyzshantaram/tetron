926a5ec7e60a199d147d44aa3e13bebc03527ccb
Siddharth Singh <me@shantaram.xyz>
2025-05-28 06:57:42 +0530

    move behaviours to single file for now

---
+++ src/engine/behaviours.rs
@@ -0,0 +1,3 @@
+use super::{entity::EntityRef, world::WorldRef};
+
+pub trait Behaviour: std::fmt::Debug {}
--- src/engine/behaviours/mod.rs
+++
@@ -1,9 +0,0 @@
-use super::{entity::EntityRef, world::WorldRef};
-
-pub struct BehaviourContext {
-    pub entity: EntityRef,
-    pub world: WorldRef,
-    pub dt: f64,
-}
-
-pub trait Behaviour: std::fmt::Debug {}
