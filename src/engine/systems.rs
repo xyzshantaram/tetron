256afe4b79582dd7836aad0d7aa13de6edc97bb0
Siddharth Singh <me@shantaram.xyz>
2025-05-28 11:26:48 +0530

    impl rune getters for ctx stuff and begin implementing query()

--- src/engine/systems.rs
+++ src/engine/systems.rs
@@ -1,8 +1,12 @@
-use super::world::WorldRef;
+use crate::TetronError;
+
+use super::{entity::EntityRef, world::WorldRef};
 
 #[derive(Clone, rune::Any)]
 pub struct Ctx {
+    #[rune(get)]
     world: WorldRef,
+    #[rune(get)]
     dt: f32,
 }
 
@@ -12,7 +16,11 @@ impl Ctx {
     }
 
     #[rune::function]
-    fn query(&self) {
-        todo!()
+    fn query(&self) -> Result<Vec<EntityRef>, TetronError> {
+        let mut result: Vec<EntityRef> = Vec::new();
+        if let Some((_, scene)) = self.world.current_scene()? {
+            for entity in scene.entities()? {}
+        }
+        Ok(result)
     }
 }
