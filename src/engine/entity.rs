514b3f3bd89509d3fe197572e678db6ffd40aaf9
Siddharth Singh <me@shantaram.xyz>
2025-05-29 09:16:46 +0530

    implement entity behaviour system

--- src/engine/entity.rs
+++ src/engine/entity.rs
@@ -4,30 +4,47 @@ use std::{
     rc::Rc,
 };
 
-use rune::{ContextError, Module};
+use crate::error::TetronError;
 
 use super::behaviours::Behaviour;
 
 #[derive(Default, Debug)]
 pub struct Entity {
-    behaviours: HashMap<String, Box<dyn Behaviour>>,
-    tags: HashSet<String>,
+    pub behaviours: HashMap<String, Behaviour>,
+    pub tags: HashSet<String>,
 }
 
 #[derive(Clone, Debug, Default, rune::Any)]
 #[rune(name = Entity)]
 pub struct EntityRef(Rc<RefCell<Entity>>);
 
-impl Entity {
-    pub fn module() -> Result<Module, ContextError> {
-        let mut module = Module::with_crate_item("tetron", ["game"])?;
-        module.ty::<EntityRef>()?;
-        Ok(module)
-    }
-}
-
 impl EntityRef {
     pub fn new() -> Self {
         EntityRef(Rc::new(RefCell::new(Entity::default())))
+    }
+
+    #[rune::function]
+    pub fn tag(&mut self, tag: &str) -> Result<(), TetronError> {
+        self.0.try_borrow_mut()?.tags.insert(tag.into());
+        Ok(())
+    }
+
+    #[rune::function]
+    pub fn has_tag(&self, tag: &str) -> Result<bool, TetronError> {
+        Ok(self.0.try_borrow()?.tags.contains(tag))
+    }
+
+    #[rune::function]
+    pub fn attach(&mut self, behaviour: Behaviour) -> Result<(), TetronError> {
+        let behaviours = &mut self.0.try_borrow_mut()?.behaviours;
+        if behaviours.contains_key(&behaviour.name) {
+            Err(TetronError::Runtime(format!(
+                "Cannot insert behaviour {}: already exists",
+                behaviour.name
+            )))
+        } else {
+            behaviours.insert(behaviour.name.clone(), behaviour);
+            Ok(())
+        }
     }
 }
